8c2a9e41fc0cdc7d199df79324d0ea5c99cae811
Kento Ogata <k.ogata1013@gmail.com>
2023-08-22 21:37:20 +0900

    refactor: Remove unused import

--- denops/@ddu-kinds/git_repo.ts
+++ denops/@ddu-kinds/git_repo.ts
@@ -1,5 +1,5 @@
 import { Actions, BaseKind } from "https://deno.land/x/ddu_vim@v3.5.1/types.ts";
-import { ActionData, KindParams } from "./git_repo/types.ts";
+import { KindParams } from "./git_repo/types.ts";
 import { find } from "./git_repo/actions/find.ts";
 
 export class Kind extends BaseKind<KindParams> {
